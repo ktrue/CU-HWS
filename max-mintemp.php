e3e2905678eaca7cd7bdad3f0f39cef6b31dbf31
MatsA <mats.ahlklo@icloud.com>
2019-01-18 19:56:56 +0100

    Update max-mintemp.php

--- max-mintemp.php
+++ max-mintemp.php
@@ -1,7 +1,30 @@
 <?php include('livedata.php');include('common.php');header('Content-type: text/html; charset=utf-8');?>
 <div class="topmin">
 
-<?php //temperture min year
+<?php //Temperature min day
+ 
+ if ($weather["temp_units"]=='C' && $weather["temp_today_low"]>30){echo "<topred1>",$weather["temp_today_low"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
+ else if ($weather["temp_units"]=='C' && $weather["temp_today_low"]>=24){echo "<toporange1>",$weather["temp_today_low"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
+ else if ($weather["temp_units"]=='C' && $weather["temp_today_low"]>=18){echo "<topyellow1>",$weather["temp_today_low"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
+ else if ($weather["temp_units"]=='C' && $weather["temp_today_low"]>=6){ echo "<topgreen1>", $weather["temp_today_low"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
+ else if ($weather["temp_units"]=='C' && $weather["temp_today_low"]>-50){ echo "<topblue1>", $weather["temp_today_low"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
+ 
+ if ($weather["temp_units"]=='F' && $weather["temp_today_low"]>86){echo "<topred1>",$weather["temp_today_low"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
+ else if ($weather["temp_units"]=='F' && $weather["temp_today_low"]>=75){echo "<toporange1>",$weather["temp_today_low"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
+ else if ($weather["temp_units"]=='F' && $weather["temp_today_low"]>=64){echo "<topyellow1>",$weather["temp_today_low"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
+ else if ($weather["temp_units"]=='F' && $weather["temp_today_low"]>=42.8){ echo "<topgreen1>", $weather["temp_today_low"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
+ else if ($weather["temp_units"]=='F' && $weather["temp_today_low"]>-50){ echo "<topblue1>", $weather["temp_today_low"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
+
+ ?>
+</div></smalluvunit>
+<div class="minword">Min</div></div>
+<div class="mintimedate">Today
+</div>  
+<div class="yearwordbig">Temp</div>
+
+<div class="topmax">
+<?php //Temperature max day
+ 
  if ($weather["temp_units"]=='C' && $weather["temp_today_high"]>30){echo "<topred1>",$weather["temp_today_high"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
  else if ($weather["temp_units"]=='C' && $weather["temp_today_high"]>=24){echo "<toporange1>",$weather["temp_today_high"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
  else if ($weather["temp_units"]=='C' && $weather["temp_today_high"]>=18){echo "<topyellow1>",$weather["temp_today_high"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
@@ -12,31 +35,11 @@
  else if ($weather["temp_units"]=='F' && $weather["temp_today_high"]>=75){echo "<toporange1>",$weather["temp_today_high"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
  else if ($weather["temp_units"]=='F' && $weather["temp_today_high"]>=64){echo "<topyellow1>",$weather["temp_today_high"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
  else if ($weather["temp_units"]=='F' && $weather["temp_today_high"]>=42.8){ echo "<topgreen1>", $weather["temp_today_high"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
- else if ($weather["temp_units"]=='F' && $weather["temp_today_high"]>-50){ echo "<topblue1>", $weather["temp_today_high"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
- ?>
-</div></smalluvunit>
-<div class="minword">Hi</div></div>
-<div class="mintimedate">Today
-</div>  
-<div class="yearwordbig">Temp</div>
-
-<div class="topmax">
-<?php //temperture max year celsius
- if ($weather["temp_units"]=='C' && $weather["temp_today_high"]>30){echo "<topred1>",$weather["temp_today_high"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
- else if ($weather["temp_units"]=='C' && $weather["tempymax"]>=24){echo "<toporange1>",$weather["tempymax"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
- else if ($weather["temp_units"]=='C' && $weather["tempymax"]>=18){echo "<topyellow1>",$weather["tempymax"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
- else if ($weather["temp_units"]=='C' && $weather["tempymax"]>=6){ echo "<topgreen1>", $weather["tempymax"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
- else if ($weather["temp_units"]=='C' && $weather["tempymax"]>-50){ echo "<topblue1>", $weather["tempymax"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
- 
- if ($weather["temp_units"]=='F' && $weather["temp_today_high"]>86){echo "<topred1>",$weather["temp_today_high"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
- else if ($weather["temp_units"]=='F' && $weather["tempymax"]>=75){echo "<toporange1>",$weather["tempymax"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
- else if ($weather["temp_units"]=='F' && $weather["tempymax"]>=64){echo "<topyellow1>",$weather["tempymax"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
- else if ($weather["temp_units"]=='F' && $weather["tempymax"]>=42.8){ echo "<topgreen1>", $weather["tempymax"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
- else if ($weather["temp_units"]=='F' && $weather["tempymax"]>-50){ echo "<topblue1>", $weather["tempymax"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; }
+ else if ($weather["temp_units"]=='F' && $weather["temp_today_high"]>-50){ echo "<topblue1>", $weather["temp_today_high"]  ;echo "&deg;<smalluvunit>".$weather["temp_units"] ; } 
 
 ?>
 </div></smalluvunit>
-<div class="maxword">Min</div></div>
+<div class="maxword">Max</div></div>
 <div class="maxtimedate">Today</oorange></div> 
 
 
