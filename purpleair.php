d88d9fce42a69e43202b2ca6f87f2564ac46bb4e
ktrue <webmaster@saratoga-weather.org>
2019-01-31 08:59:10 -0800

    Update airqualitymodule/purpleair from Meteobridge dist. Note that the two modules are primarily English only. Translation adjustments will be made at a later date.  purpleair.php is English only.

--- purpleair.php
+++ purpleair.php
@@ -1,9 +1,10 @@
-<?php include('common.php'); include('livedata.php'); $weather["cloudbase3"] = round((anyToC($weather["temp"]) - anyToC($weather["dewpoint"])) * 1000 /2.4444) ;
+<?php include('livedata.php');include('common.php'); $weather["cloudbase3"] = round((anyToC($weather["temp"]) - anyToC($weather["dewpoint"])) * 1000 /2.4444) ;
 $locationinfo='<svg id="i-location2" viewBox="0 0 32 32" width="15px" height="15px" fill="none" stroke="rgba(255, 124, 57, 1.000)" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
 <circle cx="16" cy="11" r="4" /><path d="M24 15 C21 22 16 30 16 30 16 30 11 22 8 15 5 8 10 2 16 2 22 2 27 8 24 15 Z" /></svg>';
 $alert="<svg id='firealert' viewBox='0 0 32 32' width='18px' height='18px' fill='none' stroke='rgba(255, 124, 57, 1.000)' stroke-linecap='round' stroke-linejoin='round' stroke-width='2'>
 <path d='M16 3 L30 29 2 29 Z M16 11 L16 19 M16 23 L16 25' /></svg>";
 //weather AIR QUALITY / CLOUDBASE MODULE APRIL 2018
+// modified for languages - ktrue - 30-Jan-2019
 header('Content-type: text/html; charset=UTF-8');
 ?>
 <!DOCTYPE html>
@@ -22,16 +23,21 @@ actual{font-size:13px;float:right;position:absolute;left:135px;top:20px;-webkit-
 .hitempy{background:rgba(61, 64, 66, 0.8);color:#aaa;font-size:12px;width:240px;padding:1px;border-radius:4px;margin-top:2px;margin-left:0;padding-left:3px;}
 .lotemp{font-size:26px;font-family:-apple-system, BlinkMacSystemFont, "weathertext", Roboto, Helvetica, Arial, sans-serif;font-weight:300;}
 blue{color:rgba(0, 154, 171, 1.000)}.icon{position:absolute;right:10px;bottom:10px;}
-.weather34i-cloud-bar{background:0;position:absolute;height:100px;width:100px;margin-left:135px;margin-top:48px}
+.weather34i-cloud-bar{background:0;position:absolute;height:100px;width:100px;margin-left:135px;margin-top:8px}
 .weather34i-cloud-bar .bar{shape-rendering:crispEdges;background:url(css/rain/cloudmarker.svg) no-repeat;width:100px;border:5px solid rgba(57, 61, 64, 1.00);border-bottom:3px solid rgba(57, 61, 64, 1.00);border-top:1px dotted rgba(57, 61, 64, 1.00);-webkit-border-radius:1px 1px 3px 3px;-moz-border-radius:1px 1px 3px 3px;-o-border-radius:1px 1px 3px 3px;-ms-border-radius:1px 1px 3px 3px;border-radius:1px 1px 3px 3px;position:absolute;bottom:0}
 .weather34i-cloud-bar .bar-1{height:100px;max-height:100px}
 .weather34i-cloud-bar .bar-inner{shape-rendering:crispEdges;background:rgba(159, 163, 166, 0.4);width:100%;-webkit-border-radius:1px 1px 3px 3px;-moz-border-radius:1px 1px 3px 3px;-o-border-radius:1px 1px 3px 3px;-ms-border-radius:1px 1px 3px 3px;border-radius:1px 1px 3px 3px;border:0;border-top:1px dotted rgba(255, 124, 57, 1.000)}
-.weather34icloudair{color:rgba(255, 124, 57, 1.000);position:absolute;margin-left:165px;margin-top:67px;font-family:-apple-system, BlinkMacSystemFont, "weathertext", Roboto, Helvetica, Arial, sans-serif;max-height:42px;font-weight:normal;background:#2a2e33;width:42px;height:42px;-webkit-border-radius:100%;-moz-border-radius:100%;-o-border-radius:100%;-ms-border-radius:100%;border-radius:100%;font-size:15px;line-height:10px;padding-top:13px;border:.12rem solid rgba(95,96,97,.9);}
-.weather34icloudair span{font-family:-apple-system, BlinkMacSystemFont, "weathertext", Roboto, Helvetica, Arial, sans-serif;font-size:0.8em;font-weight:normal;margin-left:-2px;}
+.weather34icloudair{color:rgba(255, 124, 57, 1.000);position:absolute;margin-left:165px;margin-top:67px;font-family: "weathertext",Helvetica, Arial, sans-serif;max-height:42px;font-weight:normal;background:#2a2e33;width:42px;height:42px;-webkit-border-radius:100%;-moz-border-radius:100%;-o-border-radius:100%;-ms-border-radius:100%;border-radius:100%;font-size:15px;line-height:10px;padding-top:13px;border:.12rem solid rgba(95,96,97,.9);}
+.weather34icloudair span{font-family: "weathertext",Helvetica, Arial, sans-serif;font-size:0.8em;font-weight:normal;margin-left:-2px;}
 
-.weather34icloudair1{color:rgba(255, 124, 57, 1.000);position:absolute;margin-left:165px;margin-top:70px;font-family:-apple-system, BlinkMacSystemFont, "weathertext", Roboto, Helvetica, Arial, sans-serif;max-height:42px;font-weight:normal;background:#2a2e33;width:41px;height:42px;-webkit-border-radius:100%;-moz-border-radius:100%;-o-border-radius:100%;-ms-border-radius:100%;border-radius:100%;font-size:13px;line-height:10px;padding-top:5px;border:.12rem solid rgba(95,96,97,.9);padding-left:3px;}
-.weather34icloudair1 span{font-family:-apple-system, BlinkMacSystemFont, "weathertext", Roboto, Helvetica, Arial, sans-serif;font-size:0.7em;font-weight:normal;margin-left:7px;color:#aaa;}
+.weather34icloudair1{color:rgba(255, 124, 57, 1.000);position:absolute;margin-left:165px;margin-top:47px;font-family: "weathertext",Helvetica, Arial, sans-serif;max-height:42px;font-weight:normal;background:#2a2e33;width:41px;height:42px;-webkit-border-radius:100%;-moz-border-radius:100%;-o-border-radius:100%;-ms-border-radius:100%;border-radius:100%;font-size:14px;line-height:10px;padding-top:13px;border:.12rem solid rgba(95,96,97,.9);padding-left:3px;}
+.weather34icloudair1 span{font-family: "weathertext",Helvetica, Arial, sans-serif;font-size:0.7em;font-weight:normal;margin-left:5px;color:#aaa;}
 
+.weather34-aqi-bar{background:0;position:absolute;height:100px;width:100px;margin-left:135px;margin-top:48px}
+.weather34-aqi-bar .bar{shape-rendering:crispEdges;background:0;width:100px;border:0;position:absolute;bottom:0}
+.weather34-aqi-bar .bar-1{height:100px;max-height:100px}
+.weather34aqi{color:#ff8841;position:absolute;margin-left:36px;margin-top:17px;font-size:12px;width:20px;font-family: "weathertext",Helvetica, Arial, sans-serif;max-height:100px;line-height:10px;font-weight:normal}
+.weather34aqi span{color:#777;font-family:weathertext,arial,sans-serif;font-size:12px}
 .weather34-aqi-bar{background:0;position:absolute;height:100px;width:100px;margin-left:135px;margin-top:48px}
 .weather34-aqi-bar .bar{shape-rendering:crispEdges;background:0;width:100px;border:0;position:absolute;bottom:0}
 .weather34-aqi-bar .bar-1{height:100px;max-height:100px}
@@ -64,6 +70,8 @@ aqiimage1{position:absolute;left:-5px;top:-2px;}
 span11{position:absolute;margin-top:60px;font-size:16px;line-height:14px;width:200px;left:135px;}airdescription{position:absolute;font-size:16px;left:48px;text-align:left;width:100px;margin-top:20px;border:0;padding:0;text-align:center;}indoorpurple{color:#a475cb;}indoorblue,indoorgreen,indoororange,indoorred,indooryellow{font-family:weathertext,helvetica,arial}indoorred{color:rgba(211,93,78,1)}indoororange{color:#ff8841}indoorgreen{color:#9aba2f}indoorblue{color:#01a4b4}indooryellow{color:rgba(233, 171, 74, 1.000)}.lotemp1{font-size:22px;font-family:-apple-system, BlinkMacSystemFont, "weathertext", Roboto, Helvetica, Arial, sans-serif;font-weight:300;}
 
 
+.indoorblue1,.indoorgreen1,.indoororange1,.indoorred1,.indooryellow1,.indoorpurple1{font-family:'weathertext',Arial,Helvetica,system;width:4.9rem;height:4.4rem;font-size:1.4rem;padding-top:7px;color:#333;background:rgba(59, 156, 172, 1.000);border-bottom: 10px solid rgba(56, 56, 60, 1);display:flex;align-items:center;justify-content:center;-webkit-border-radius: 3px;-moz-border-radius: 3px;-o-border-radius: 3px;border-radius: 3px;position:absolute;margin-left:-10px;top:-5px}
+.indoorred1{background:rgba(211,93,78,1)}.indoororange1{background:#ff8841}.indoorgreen1{background:#9aba2f}.indoorblue1{background:#01a4b4}.indooryellow1{background:rgba(233,171,74,1)}.indoorpurple1{background:#a475cb}
 
 .circlegreen{padding-top:10px;padding-left:5px;background:#9aba2f; -webkit-border-radius:100%;-moz-border-radius:100%;-o-border-radius:100%;border-radius:100%;width:45px;height:45px;position:absolute;margin-top:0;color:#fff;line-height:28px;color:#fff;}
 .circleyellow{padding-top:10px;padding-left:5px;background:rgba(233, 171, 74, 1); -webkit-border-radius:100%;-moz-border-radius:100%;-o-border-radius:100%;border-radius:100%;width:45px;height:45px;position:absolute;margin-top:0;color:#fff;line-height:28px;}
@@ -71,7 +79,21 @@ span11{position:absolute;margin-top:60px;font-size:16px;line-height:14px;width:2
 .circlered{padding-top:10px;padding-left:5px;background:rgba(211,93,78,1); -webkit-border-radius:100%;-moz-border-radius:100%;-o-border-radius:100%;border-radius:100%;width:45px;height:45px;position:absolute;margin-top:0;color:#fff;line-height:28px;}
 .circlepurple{padding-top:10px;padding-left:5px;;background:#a475cb; -webkit-border-radius:100%;-moz-border-radius:100%;-o-border-radius:100%;border-radius:100%;width:45px;height:45px;position:absolute;margin-top:0;color:#fff;line-height:28px;}
 .airwarning{position:absolute;margin-left:160px;margin-top:70px;font-size:0.9rem;}.airwarning1{position:absolute;margin-left:225px;margin-top:75px;}
-.weather34darkbrowser{position:relative;background:0;width:104%;max-height:30px;margin:auto;margin-top:-15px;margin-left:-20px;border-top-left-radius:5px;border-top-right-radius:5px;padding-top:45px;background-image:radial-gradient(circle,#EB7061 6px,transparent 8px),radial-gradient(circle,#F5D160 6px,transparent 8px),radial-gradient(circle,#81D982 6px,transparent 8px),radial-gradient(circle,rgba(97,106,114,1) 2px,transparent 2px),radial-gradient(circle,rgba(97,106,114,1) 2px,transparent 2px),radial-gradient(circle,rgba(97,106,114,1) 2px,transparent 2px),linear-gradient(to bottom,rgba(59,60,63,0.4) 40px,transparent 0);background-position:left top,left top,left top,right top,right top,right top,0 0;background-size:50px 45px,90px 45px,130px 45px,50px 30px,50px 45px,50px 60px,100%;background-repeat:no-repeat,no-repeat}.weather34darkbrowser[url]:after{content:attr(url);color:#aaa;font-size:14px;position:absolute;left:0;right:0;top:0;padding:2px 15px;margin:11px 50px 0 90px;border-radius:3px;background:rgba(97, 106, 114, 0.3);height:20px;box-sizing:border-box}
+.weather34darkbrowser{position:relative;background:0;width:104%;max-height:30px;margin:auto;margin-top:-15px;margin-left:-20px;border-top-left-radius:5px;border-top-right-radius:5px;padding-top:45px;background-image:radial-gradient(circle,#EB7061 6px,transparent 8px),radial-gradient(circle,#F5D160 6px,transparent 8px),radial-gradient(circle,#81D982 6px,transparent 8px),radial-gradient(circle,rgba(97,106,114,1) 2px,transparent 2px),radial-gradient(circle,rgba(97,106,114,1) 2px,transparent 2px),radial-gradient(circle,rgba(97,106,114,1) 2px,transparent 2px),linear-gradient(to bottom,rgba(59,60,63,0.4) 40px,transparent 0);background-position:left top,left top,left top,right top,right top,right top,0 0;background-size:50px 45px,90px 45px,130px 45px,50px 30px,50px 45px,50px 60px,100%;background-repeat:no-repeat,no-repeat}.weather34darkbrowser[url]:after{content:attr(url);color:#aaa;font-size:14px;position:absolute;left:0;right:0;top:0;padding:2px 15px;margin:11px 50px 0 90px;border-radius:3px;background:rgba(97, 106, 114, 0.3);height:20px;box-sizing:border-box}smalluvunit{position:absolute;display:inline;font-size:.85rem;font-family:Arial,Helvetica,system;vertical-align:top}
+.air0,.air50,.air100,.air150,.air200,.air300{font-family:'weathertext',Arial,Helvetica,system;width:8rem;height:7.68rem;font-size:1.50rem;padding-top:0;color:#fff;border-bottom: 15px solid rgba(56, 56, 60, 1);display:flex;align-items:center;justify-content:center;-webkit-border-radius: 3px;-moz-border-radius: 3px;-o-border-radius: 3px;border-radius: 3px;margin-left:40px;margin-top:20px;}
+.air0{background:rgba(144, 177, 42, 1.000)}
+.air50{background:rgba(230, 161, 65, 1.000)}
+.air100{background:rgba(255, 124, 57, 0.8)}
+.air150{background:rgba(211, 93, 78, 0.8)}
+.air200{background:#a475cb}
+.air250{background:#a475cb}
+.air300{background:#a475cb}
+
+.cloudbase0,.cloudbase1500{font-family:'weathertext',Arial,Helvetica,system;width:8rem;height:7.68rem;font-size:1.70rem;padding-top:0;color:#fff;border-bottom: 15px solid rgba(56, 56, 60, 1);display:flex;align-items:center;justify-content:center;-webkit-border-radius: 3px;-moz-border-radius: 3px;-o-border-radius: 3px;border-radius: 3px;margin-left:-40px;margin-top:15px;}
+.cloudbase0{background:rgba(68, 166, 181, 1.000)}
+.cloudbase1500{background:rgba(230, 161, 65, 1.000)}
+.estimated{position:absolute;font-size:0.9rem;margin-top:80px;left:-20px;}
+
 </style>
 </head>
 <body>
@@ -79,39 +101,49 @@ span11{position:absolute;margin-top:60px;font-size:16px;line-height:14px;width:2
 
 
 
+           
 <section class="weather34cards">
    <div class="weather34card weather34card--earthquake1">
                <div class="weather34card_weather34-container">
                
             <div class="weather34card_weather34-wrapper"><span class="weather34card__count-text--big">
+            <div class="lotemp1">Cloudbase</div>
               <div class="weather34i-cloud-bar">
-<div class="bar bar-1" style="height:100px;max-height:100px;"> 
-<?php //cloudbase weather34
-echo '<div class="bar bar-inner" style="max-height:90px;height: ';echo $weather["cloudbase3"]/100;?>px;">
-</div></div></div>
-<div class="weather34icloudair1">
-   
-<?php  
- echo "&nbsp; ",$weather["cloudbase3"] ;echo "<span> feet </span>";
- ?>
-</span></div> 
-</div>
  <div class="lotemp">
-<?php //cloudbase
-	echo $weather["cloudbase3"], "<orange> ft</orange><br>"; echo round($weather["cloudbase3"]*0.3048,0), "<orange> mt</orange>";?>
-  <div class="icon"><img src=css/icons/temp34.svg width=25px></div>
+<div class="aqigraphic">
+<div class="airqualitymoduleposition">
+
+<?php //WEATHER34 AIR QAULITY SVG
+if ($weather["cloudbase3"]>=1500){
+	echo "<div class=cloudbase1500>";
+} else {
+	echo "<div class=cloudbase0>";
+}
+
+if($weather["temp_units"] == 'F') {
+	echo $weather["cloudbase3"]."&nbsp; ft<br>"; 
+	echo round($weather["cloudbase3"]*0.3048,0)."&nbsp; m";  
+} else {
+	echo round($weather["cloudbase3"]*0.3048,0)."&nbsp; m<br>";  
+	echo $weather["cloudbase3"]."&nbsp; ft"; 
+}
+
+
+?>
+</div></div></div>
+<div class="estimated">Estimated</div>
   
   
-  
-  </div></div>    
+ 
+  </div>
+     
      <div class="weather34card__count-container">
      <div class="weather34card__count-text"></div></div>
      <div class="weather34card__stuff-container">            
 <div class="weather34card__stuff-text"> 	
 </div></div>
-<actual><?php echo date('jS D H:i');?></actual>
+
 </div></div></div></div>
-    
        
     <div class="weather34card weather34card--earthquake2">
                <div class="weather34card_weather34-container">
@@ -166,16 +198,18 @@ $a="";if($aqiweather["aqi"]==$a){$aqiweather["aqi"] = "0" ;}
 <div class="lotemp1">Air Quality</div>
 </div>
 <div class="aqigraphic">
-<div class="airhouse">
+<div class="airqualitymoduleposition">
+
 <?php //WEATHER34 AIR QAULITY SVG
-if ($aqiweather["aqi"]>300){echo "<img src='css/aqi/hazair.svg?ver=1.4' width='120px' height='120px' alt='weather34 hazardous air quality' title='weather34 hazardous air quality' "; }
-else if ($aqiweather["aqi"]>200){echo "<img src='css/aqi/vhair.svg?ver=1.4' width='120px' height='120px' alt='weather34 very unhealthy air quality' title='weather34 very unhealthy air quality'" ; }
-else if ($aqiweather["aqi"]>150){echo "<img src='css/aqi/uhair.svg?ver=1.4' width='120px' height='120px' alt='weather34 unhealthy air quality' title='weather34 unhealthy air quality'" ; }
-else if ($aqiweather["aqi"]>100){echo "<img src='css/aqi/uhfsair.svg?ver=1.4' width='120px' height='120px'  alt='weather34 unhealthy for some air quality' title='weather34 unhealthy for some air quality'" ; }
-else if ($aqiweather["aqi"]>50){echo "<img src='css/aqi/modair.svg?ver=1.4' width='120px' height='120px' alt='weather34 moderate air quality' title='weather34 moderate air quality'" ; }
-else if ($aqiweather["aqi"]>=0){echo "<img src='css/aqi/goodair.svg?ver=1.4' width='120px' height='120px' alt='weather34 good air quality' title='weather34 good air quality'" ; }
+if ($aqiweather["aqi"]>300){echo "<div class=\"air300\"><img src=\"css/aqi/hazair.svg?ver=1.4\" width=\"110\" height=\"100\" alt=\"weather34 hazardous air quality\" title=\"weather34 hazardous air quality\" "; }
+else if ($aqiweather["aqi"]>200){echo "<div class=\"air200\"><img src=\"css/aqi/vhair.svg?ver=1.4\" width=\"110\" height=\"100\" alt=\"weather34 very unhealthy air quality\" title=\"weather34 very unhealthy air quality\"" ; }
+else if ($aqiweather["aqi"]>150){echo "<div class=\"air150\"><img src=\"css/aqi/uhair.svg?ver=1.4\" width=\"110\" height=\"100\" alt=\"weather34 unhealthy air quality\" title=\"weather34 unhealthy air quality\"" ; }
+else if ($aqiweather["aqi"]>100){echo "<div class=\"air100\"><img src=\"css/aqi/uhfsair.svg?ver=1.4\" width=\"110\" height=\"100\"  alt=\"weather34 unhealthy for some air quality\" title=\"weather34 unhealthy for some air quality\"" ; }
+else if ($aqiweather["aqi"]>50){echo "<div class=\"air50\"><img src=\"css/aqi/modair.svg?ver=1.4\" width=\"110\" height=\"100\" alt=\"weather34 moderate air quality\" title=\"weather34 moderate air quality\"" ; }
+else if ($aqiweather["aqi"]>=0){echo "<div class=\"air0\"><img src=\"css/aqi/goodair.svg?ver=1.4\" width=\"110\" height=\"100\" alt=\"weather34 good air quality\" title=\"weather34 good air quality\"" ; }
+
 ?>
-</div></div>
+</div></div></div>
   
 
 <?php //WEATHER34 AIR QAULITY VALUE 
@@ -199,12 +233,12 @@ else if ($aqiweather["aqi"]>=0){echo "<img src='css/aqi/goodair.svg?ver=1.4' wid
 </div>
 
 <div class="airwarning1"><?php 
-if ($aqiweather["aqi"]>300){echo "<div class=circlered>PM2.5" ; }
-else if ($aqiweather["aqi"]>200){echo "<div class=circlepurple>PM2.5" ; }
-else if ($aqiweather["aqi"]>150){echo "<div class=circlered>PM2.5" ; }
-else if ($aqiweather["aqi"]>100){echo "<div class=circleorange>PM2.5" ; }
-else if ($aqiweather["aqi"]>50){echo "<div class=circleyellow>PM2.5" ; }
-else if ($aqiweather["aqi"]>=0){echo "<div class=circlegreen>PM2.5" ; }
+if ($aqiweather["aqi"]>300){echo "<div class=\"indoorred1\">PM2.5" ; }
+else if ($aqiweather["aqi"]>200){echo "<div class=\"indoorpurple1\">PM2.5" ; }
+else if ($aqiweather["aqi"]>150){echo "<div class=\"indoorred1\">PM2.5" ; }
+else if ($aqiweather["aqi"]>100){echo "<div class=\"indoororange1\">PM2.5" ; }
+else if ($aqiweather["aqi"]>50){echo "<div class=\"indooryellow1\">PM2.5" ; }
+else if ($aqiweather["aqi"]>=0){echo "<div class=\"indoorgreen1\">PM2.5" ; }
 ?></div>
 
         </div>
@@ -267,7 +301,7 @@ else if ($aqiweather["aqi"]>=0){echo "<div class=circlegreen>PM2.5" ; }
             
 <actual1>Information on Air Quality </actual1>     
 
-<div class="icon"><img src=img/purpleair.svg width=30px alt="https://purpleair.com/" title="https://purpleair.com/"></div></div></div></div></div>
+<div class="icon"><img src="img/purpleair.svg" width="30" alt="https://purpleair.com/" title="https://purpleair.com/"></div></div></div></div></div>
 </section>
 <div class="provided">   
 &nbsp;
